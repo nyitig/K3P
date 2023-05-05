e32c74ff984871c491066a9f83a0b71cdfe0051f
NyitiG <nyitigvilaga@gmail.com>
2023-05-05 13:54:02 +0200

    add group done
    dashboard.css:
    -create new class for add group
    
    dashboard.js:
    - create new functions:
      - date to milisec
      -  addNewGroupBtnClick
      - addNewGroup
      - addNewGroupsCheck
      - getDataIds
      - btnCancel
    
    change functions:
    - groupDelete
    -

--- assets/dashboard/dashboard.js
+++ assets/dashboard/dashboard.js
@@ -670,13 +670,18 @@ function showHideEntries() {
   return
 }
 
-// date convers functions
+// date converts functions
 
 function dateConvertToReadableDate(dateTime) {
   let msTime=dateTime
   let time= new Date().getTime(msTime)
   let date= new Date(time)
   return date.toString()
+}
+function dataTimeToMilisec(dataTimeInput) {
+  let newDataTime=dataTimeInput.value
+  let dataTimeMilisec= Date.parse(`${newDataTime}`)
+  return dataTimeMilisec
 }
 
 // DR target tree create
@@ -875,6 +880,7 @@ function reloadNewKdbxData() {
 function groupActionBtnActivated() {
   groupDeleteBtnClick()
   moveGroupBtnClick()
+  addNewGroupBtnClick()
   return
 }
 
@@ -940,7 +946,17 @@ function getJwtToken() {
 
 //Add new group
 
-function addNewGroup() {
+function addNewGroupBtnClick() {
+  const groupsAddBtns=document.querySelectorAll('.groupsAdd')
+  for (let i = 0; i < groupsAddBtns.length; i++) {
+    groupsAddBtns[i].addEventListener('click',()=>{
+      addNewGroup(groupsAddBtns[i])
+    })    
+  }
+  return
+}
+
+function addNewGroup(btn) {
   /*
   Ezek  a paraméterek kellenek:
   "kdbxFileDto":
@@ -957,8 +973,147 @@ function addNewGroup() {
         "groupNameDto": "IT First"
     }
 }
+
+expiresDto: boolean érték
+groupExpiryTimeDto: ms-ben
   */ 
+/*
+Mit is kell csinálni?
+- azonosítani, h melyik grouphoz szeretnék hozzáadni => kész
+- templatet létrehozni, majfd megjeleníteni az oinput sectionban => kész
+  -template tartalma: expiresTime, name, add, cancel btn => kész
+- aktiválni a btn-et => kész
+  - ha cancel btn=> inputsectionhide => kész
+  - ha add => ellenőrizni, h minden adat beírásra került-e
+    - ha nem=>wrongtemplate+ wrongpanel activated
+    - ha minden ok=> létrehozni afethez az url, options-t => groupActions(url,options).then(result=>resultFunctions(result))
+
+*/  
+let dataIds=getDataIds(btn)
+let addGroupName=targetNameKeys[dataIds]
+let addGroupTemplate=` <div id="addGroupDiv" class="column groupActions">
+<h2 class="inputSectionH2">Add group</h2>
+<h3 class="inputSectionH3">Group name: ${addGroupName} </h3>
+<div class="addNameDiv row alItCent inputDiv">
+  <label for="groupNames" class="inputLabels">Name of the new group:</label>
+  <input type="text" name="groupNames" id="addGroupNameInput" placeholder="New group name..." autocomplete="off">
+</div>
+<div  class=" addExpChkbxDiv row alItCent inputDiv">
+  <label for="groupExp" class="inputLabels">add an expiration date</label>
+  <input type="checkbox" name="groupExp" id="addGroupExpChkbx">
+</div>
+<div id="addExpDiv" class=" row alItCent inputDiv">
+  <label for="groupExp" class="inputLabels">expiration date of the new group:</label>
+  <input type="datetime-local" name="groupExp" id="addGroupExpInput">
+</div>
+<div id="addBtnDiv" class="row inputDiv">
+<button id="btnAddGroup" class="btnAll btnActions" >Add</button>
+<button id="btnCancel" class="btnAll ">Cancel</button>
+</div>
+`
+inputSectionLoadTemplate(addGroupTemplate)
+inputSectionShow()
+btnCancel()
+// collecting data to be sent
+const addGroupExpChkbx=document.getElementById('addGroupExpChkbx')
+const addExpDiv=document.getElementById('addExpDiv')
+const addGroupExpInput=document.getElementById('addGroupExpInput')
+let groupExpiryTimeDto=""
+let expiresDto=false
+addGroupExpChkbx.addEventListener('click',()=>{
+  if (!addGroupExpChkbx.checked) {
+    addExpDiv.classList.remove('active')
+    expiresDto=false
+    addGroupExpInput.value=""
+    groupExpiryTimeDto=""
+  }
+  if (addGroupExpChkbx.checked) {
+    addExpDiv.classList.add('active')
+    expiresDto=true
+  }
+})
+
+addGroupExpInput.addEventListener('change',()=>{
+ let d=dataTimeToMilisec(addGroupExpInput)
+ groupExpiryTimeDto=d
+})
+let targetGroupDirectionDto=targetDRKeys[dataIds]
+let groupNameDto=""
+const addGroupNameInput=document.getElementById('addGroupNameInput')
+addGroupNameInput.addEventListener("keyup",()=>{
+  groupNameDto=addGroupNameInput.value
+})
+// add btn click
+const btnAddGroup=document.getElementById('btnAddGroup')
+btnAddGroup.addEventListener("click",()=>{
+  let checkOk=checkList()
+  checkOk=addNewGroupsCheck(expiresDto,groupExpiryTimeDto,groupNameDto)
+  if (!checkOk) {
+    return
+  }
+  if (checkOk) {
+  const urlAdd="http://127.0.0.1:9933/api/kdbx/1/groups"
+  const jwtToken=getJwtToken()
+  let fetchbody={       
+    "kdbxFileDto":
+    {
+        "kdbxFilePwDto": "1"
+    },
+    
+    "groupDto":
+    {
+        "expiresDto": "",
+        "groupExpiryTimeDto": "",
+        
+        "targetGroupDirectionDto": `${targetGroupDirectionDto}`,
+        "groupNameDto": `${groupNameDto}`
+    }
 }
+const options= {
+  method:'POST',
+   body:JSON.stringify(fetchbody),
+  headers: {
+      'Content-Type': 'application/json',
+      'Accept' : 'application/json',
+      'Authorization': `Bearer ${jwtToken}`,
+    },
+}
+groupActions(urlAdd,options)
+.then(result=>{resultFunctions(result)})
+
+  }
+})
+
+return
+}
+
+function addNewGroupsCheck(expiresDto,groupExpiryTimeDto,groupNameDto) {
+  let checkStatus=true
+  if (expiresDto) {
+    if (groupExpiryTimeDto=="") {
+      checkStatus=false
+      let wrongTemplate=`<h2>Expiration date is missing!</h2>`
+      showWrongPanel(wrongTemplate)
+      setTimeout(() => {
+        hideWrongPanel()
+      }, 2000);
+      return checkStatus
+    }
+  }
+  if (groupNameDto=="") {
+    checkStatus=false
+    let wrongTemplate=`<h2>You didn't enter a name!</h2>`
+    showWrongPanel(wrongTemplate)
+    setTimeout(() => {
+      hideWrongPanel()
+    }, 2000);
+    return checkStatus
+  }
+  return checkStatus
+}
+
+// Edit group
+
 
 // Move Group
 
@@ -1008,7 +1163,7 @@ function moveGroup(btn) {
       - van net kapcsolat? => kész
       -   
   */ 
-  let dataIds=btn.attributes[1].value
+  let dataIds=getDataIds(btn)
   let moveGroupName=targetNameKeys[dataIds]
   let sourceGroupDirectionDto=targetDRKeys[dataIds]
   let moveTemplate=`
@@ -1106,7 +1261,14 @@ function groupDeleteBtnClick() {
   const groupsDeleteBtns=document.querySelectorAll('.groupsDelete')
   for (let i = 0; i < groupsDeleteBtns.length; i++) {
     groupsDeleteBtns[i].addEventListener('click',()=>{
-      /* - Miket kell csinálni?
+          groupDelete(groupsDeleteBtns[i])
+    })
+  }
+return
+}
+
+function groupDelete(btn) {
+        /* - Miket kell csinálni?
         - azonosítani kell a DR-t és a group name-t => kész
         - az input section-t fel kell tölteni a deletéhez szükséges adatokkal => kész
           - név => kész
@@ -1114,60 +1276,57 @@ function groupDeleteBtnClick() {
             -törlés esetén: újra betölteni a komplett alsó panelt
             - mégsem esetén: zár az input panel, megjelenik a center és a right, majd az input panel innerHTML="" =>kész
       */ 
-      let dataIds=groupsDeleteBtns[i].attributes[1].value
-      let deleteGroupName=targetNameKeys[dataIds]
-      let deleteDR=targetDRKeys[dataIds]
-      let deleteTemplate=`
-      <div id="deleteGroupDiv" class="column groupActions">
-      <h2 class="inputSectionH2">Delete group</h2>
-      <h3 class="inputSectionH3">Group name: ${deleteGroupName} </h3>
-      <div id="deleteBtnDiv" class="row">
-          <button id="btnDeleteGroup" class="btnAll btnActions" >Delete</button>
-          <button id="btnCancel" class="btnAll ">Cancel</button>
-      </div>
-  </div>
-      `
-      inputSectionLoadTemplate(deleteTemplate)
-      inputSectionShow()
-      const btnCancel=document.getElementById('btnCancel')
-      btnCancel.addEventListener("click", inputSectionHide) // cancel btn click
-      const btnDeleteGroup=document.getElementById('btnDeleteGroup')
-      btnDeleteGroup.addEventListener("click",()=>{
-        let checkedOk=checkList()
-        if (!checkedOk) {
-          inputSectionHide()
-          return
-        }
-        const urlDelete="http://127.0.0.1:9933/api/kdbx/1/groups"          
-          const jwtToken=getJwtToken()
-          const fetchbody={       
-            "kdbxFileDto":
-            {
-                "kdbxFileIdDto": "2",
-                "kdbxFilePwDto": "1"
-            },
-            
-            "groupDto":
-            {
-                "targetGroupDirectionDto": `${deleteDR}`
+            let dataIds=getDataIds(btn)
+            let deleteGroupName=targetNameKeys[dataIds]
+            let deleteDR=targetDRKeys[dataIds]
+            let deleteTemplate=`
+            <div id="deleteGroupDiv" class="column groupActions">
+            <h2 class="inputSectionH2">Delete group</h2>
+            <h3 class="inputSectionH3">Group name: ${deleteGroupName} </h3>
+            <div id="deleteBtnDiv" class="row">
+                <button id="btnDeleteGroup" class="btnAll btnActions" >Delete</button>
+                <button id="btnCancel" class="btnAll ">Cancel</button>
+            </div>
+        </div>
+            `
+            inputSectionLoadTemplate(deleteTemplate)
+            inputSectionShow()
+            const btnCancel=document.getElementById('btnCancel')
+            btnCancel.addEventListener("click", inputSectionHide) // cancel btn click
+            const btnDeleteGroup=document.getElementById('btnDeleteGroup')
+            btnDeleteGroup.addEventListener("click",()=>{
+              let checkedOk=checkList()
+              if (!checkedOk) {
+                inputSectionHide()
+                return
+              }
+              const urlDelete="http://127.0.0.1:9933/api/kdbx/1/groups"          
+                const jwtToken=getJwtToken()
+                const fetchbody={       
+                  "kdbxFileDto":
+                  {
+                      "kdbxFileIdDto": "2",
+                      "kdbxFilePwDto": "1"
+                  },
+                  
+                  "groupDto":
+                  {
+                      "targetGroupDirectionDto": `${deleteDR}`
+                  }
+              }
+              const options= {
+                method:'DELETE',
+                 body:JSON.stringify(fetchbody),
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Accept' : 'application/json',
+                    'Authorization': `Bearer ${jwtToken}`,
+                  },
             }
-        }
-        const options= {
-          method:'DELETE',
-           body:JSON.stringify(fetchbody),
-          headers: {
-              'Content-Type': 'application/json',
-              'Accept' : 'application/json',
-              'Authorization': `Bearer ${jwtToken}`,
-            },
-      }
-     
-          groupActions(urlDelete,options)
-          .then(result=>{resultFunctions(result)})     
-      })
-    })
-  }
-return
+           
+                groupActions(urlDelete,options)
+                .then(result=>{resultFunctions(result)})     
+            })
 }
 
 async function groupActions(url,options) {
@@ -1201,4 +1360,13 @@ function resultFunctions(result) {
               sessionStorage.kdbx=JSON.stringify(result)
               reloadNewKdbxData()
             }
+}
+
+function getDataIds(btn) {
+  return btn.attributes[1].value
+}
+
+function btnCancel() {
+  const btnCancel=document.getElementById('btnCancel')  
+btnCancel.addEventListener("click",inputSectionHide)
 }
